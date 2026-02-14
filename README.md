25fec60b171d716fd2570c2ab54c497cbdca6753
ztao-8 <143745233+ztao-8@users.noreply.github.com>
2026-02-14 13:46:43 -0800

    changes for server storage

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # file-transfer
-
-It might work.
+## Changes of Server:
+1. To ensure there is enough space available on the disk: I check available disk memory for the given directory before server sends OK response.
+2. During streaming process, I check whether there's a failure/error.
+3. Check if there's error for receiving checksum.
+4. Respond to the client with the status of the transfer (success or failure), rather than only track logs. Also, disconnect the client
--- server/server.go
+++ server/server.go
@@ -9,33 +9,84 @@ import (
 	"log"
 	"net"
 	"os"
+	"golang.org/x/sys/unix"
+	"path/filepath"
 )
 
 func handleStorage(msgHandler *messages.MessageHandler, request *messages.StorageRequest) {
 	log.Println("Attempting to store", request.FileName)
-	file, err := os.OpenFile(request.FileName, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0666)
+
+	// Ensure the file is not already present
+	file, err := os.OpenFile(request.FileName, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0666) // Create file if it doesn't exist, error if it does
 	if err != nil {
 		msgHandler.SendResponse(false, err.Error())
 		msgHandler.Close()
 		return
 	}
+	// We should check the space of disk before writing to it
+	// If we send OK ("Ready for data"), the client will start streaming bytes immediately.
+	dir := filepath.Dir(request.FileName)
+	if dir == "" {
+		dir = "."
+	}
+	var st unix.Statfs_t
+	if err := unix.Statfs(dir, &st); err != nil {
+		msgHandler.SendResponse(false, err.Error())
+		msgHandler.Close()
+		return
+	}
+	available := uint64(st.Bavail) * uint64(st.Bsize)
+	if available < request.Size {
+		msgHandler.SendResponse(false, fmt.Sprintf("not enough disk space: need=%d available=%d", request.Size, available))
+		msgHandler.Close()
+		return
+	}
 
-	msgHandler.SendResponse(true, "Ready for data")
+	msgHandler.SendResponse(true, "Ready for data") // Send OK response to client that we are ready to receive data
 	md5 := md5.New()
 	w := io.MultiWriter(file, md5)
-	io.CopyN(w, msgHandler, int64(request.Size)) /* Write and checksum as we go */
-	file.Close()
+	if _, err := io.CopyN(w, msgHandler, int64(request.Size)); err != nil { /* Write and checksum as we go */
+		_ = file.Close()
+		_ = os.Remove(request.FileName) // Don't keep partial/corrupt file
+		_ = msgHandler.SendResponse(false, fmt.Sprintf("transfer failed while receiving data: %v", err))
+		msgHandler.Close()
+		return
+	}
+	if err := file.Close(); err != nil {
+		_ = os.Remove(request.FileName)
+		_ = msgHandler.SendResponse(false, fmt.Sprintf("failed to close file: %v", err))
+		msgHandler.Close()
+		return
+	}
 
 	serverCheck := md5.Sum(nil)
 
-	clientCheckMsg, _ := msgHandler.Receive()
+	clientCheckMsg, err := msgHandler.Receive()
+	if err != nil {
+		_ = os.Remove(request.FileName)
+		_ = msgHandler.SendResponse(false, fmt.Sprintf("failed to receive checksum: %v", err))
+		msgHandler.Close()
+		return
+	}
+	if clientCheckMsg.GetChecksum() == nil {
+		_ = os.Remove(request.FileName)
+		_ = msgHandler.SendResponse(false, "invalid checksum message")
+		msgHandler.Close()
+		return
+	}
 	clientCheck := clientCheckMsg.GetChecksum().Checksum
 
 	if util.VerifyChecksum(serverCheck, clientCheck) {
 		log.Println("Successfully stored file.")
+		_ = msgHandler.SendResponse(true, "Storage complete")
 	} else {
 		log.Println("FAILED to store file. Invalid checksum.")
+		_ = os.Remove(request.FileName)
+		_ = msgHandler.SendResponse(false, "Invalid checksum")
 	}
+
+	// Requirement: disconnect client after responding with status.
+	msgHandler.Close()
 }
 
 func handleRetrieval(msgHandler *messages.MessageHandler, request *messages.RetrievalRequest) {
