ec643c10e275d996a284075b87c3afd56365cedf
Shuqing Zou <szou5@orion01.cs.usfca.edu>
2026-02-16 16:42:53 -0800

    Fixed GET to use download-dir

--- client/client.go
+++ client/client.go
@@ -10,6 +10,7 @@ import (
 	"net"
 	"os"
 	"strings"
+	"path/filepath"
 )
 
 func put(msgHandler *messages.MessageHandler, fileName string) int {
@@ -18,22 +19,32 @@ func put(msgHandler *messages.MessageHandler, fileName string) int {
 	// Get file size and make sure it exists
 	info, err := os.Stat(fileName)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println("Error stating file:", err)
+		return 1
 	}
 
 	// Tell the server we want to store this file
-	msgHandler.SendStorageRequest(fileName, uint64(info.Size()))
+	msgHandler.SendStorageRequest(filepath.Base(fileName), uint64(info.Size()))
 	if ok, _ := msgHandler.ReceiveResponse(); !ok {
 		return 1
 	}
 
-	file, _ := os.Open(fileName)
-	md5 := md5.New()
-	w := io.MultiWriter(msgHandler, md5)
-	io.CopyN(w, file, info.Size()) // Checksum and transfer file at same time
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Println("Error opening file:", err)
+		return 1
+	}
+	defer file.Close()
+
+	h := md5.New()
+	w := io.MultiWriter(msgHandler, h)
+	if _, err := io.CopyN(w, file, info.Size()); err != nil {
+		log.Println("Error transferring file:", err)
+		return 1
+	} // Checksum and transfer file at same time
 	file.Close()
 
-	checksum := md5.Sum(nil)
+	checksum := h.Sum(nil)
 	msgHandler.SendChecksumVerification(checksum)
 	if ok, _ := msgHandler.ReceiveResponse(); !ok {
 		return 1
@@ -43,14 +54,8 @@ func put(msgHandler *messages.MessageHandler, fileName string) int {
 	return 0
 }
 
-func get(msgHandler *messages.MessageHandler, fileName string) int {
+func get(msgHandler *messages.MessageHandler, fileName string, dir string) int {
 	fmt.Println("GET", fileName)
-
-	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Println(err)
-		return 1
-	}
 
 	msgHandler.SendRetrievalRequest(fileName)
 	ok, _, size := msgHandler.ReceiveRetrievalResponse()
@@ -58,19 +63,37 @@ func get(msgHandler *messages.MessageHandler, fileName string) int {
 		return 1
 	}
 
-	md5 := md5.New()
-	w := io.MultiWriter(file, md5)
-	io.CopyN(w, msgHandler, int64(size))
+	outputPath := filepath.Join(dir, fileName)
+	file, err := os.OpenFile(outputPath, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Println(err)
+		return 1
+	}
+	defer file.Close()
+
+	h := md5.New()
+	w := io.MultiWriter(file, h)
+	if _, err := io.CopyN(w, msgHandler, int64(size)); err != nil {
+		log.Println("Error receiving file:", err)
+		return 1
+	}
 	file.Close()
 
-	clientCheck := md5.Sum(nil)
-	checkMsg, _ := msgHandler.Receive()
+	clientCheck := h.Sum(nil)
+	checkMsg, err := msgHandler.Receive()
+	if err != nil || checkMsg.GetChecksum() == nil {
+		log.Println("Failed to receive checksum")
+		return 1
+	}
 	serverCheck := checkMsg.GetChecksum().Checksum
 
 	if util.VerifyChecksum(serverCheck, clientCheck) {
 		log.Println("Successfully retrieved file.")
+		return 0
 	} else {
 		log.Println("FAILED to retrieve file. Invalid checksum.")
+		os.Remove(outputPath)
+		return 1
 	}
 
 	return 0
@@ -85,15 +108,16 @@ func main() {
 	host := os.Args[1]
 	conn, err := net.Dial("tcp", host)
 	if err != nil {
-		log.Fatalln(err.Error())
-		return
+		log.Println("Error:", err)
+		os.Exit(1)
 	}
 	msgHandler := messages.NewMessageHandler(conn)
 	defer conn.Close()
 
 	action := strings.ToLower(os.Args[2])
 	if action != "put" && action != "get" {
-		log.Fatalln("Invalid action", action)
+		log.Println("Invalid action:", action)
+        os.Exit(1)
 	}
 
 	fileName := os.Args[3]
@@ -102,11 +126,11 @@ func main() {
 	if len(os.Args) >= 5 {
 		dir = os.Args[4]
 	}
-	openDir, err := os.Open(dir)
-	if err != nil {
-		log.Fatalln(err)
+	// check if dir exits
+	if info, err := os.Stat(dir); err != nil || !info.IsDir() {
+		log.Printf("Directory %s does not exist\n", dir)
+		os.Exit(1)
 	}
-	openDir.Close()
 
 	if action == "put" {
 		os.Exit(put(msgHandler, fileName))
