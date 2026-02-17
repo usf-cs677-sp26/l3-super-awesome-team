dd5cf68290ff89aed921230cb693b483f9c1f7e6
Shuqing Zou <szou5@orion01.cs.usfca.edu>
2026-02-16 16:43:41 -0800

    Add check disk space

--- server/server.go
+++ server/server.go
@@ -9,6 +9,8 @@ import (
 	"log"
 	"net"
 	"os"
+	"path/filepath"
+	"golang.org/x/sys/unix"
 )
 
 func handleStorage(msgHandler *messages.MessageHandler, request *messages.StorageRequest) {
@@ -19,23 +21,61 @@ func handleStorage(msgHandler *messages.MessageHandler, request *messages.Storag
 		msgHandler.Close()
 		return
 	}
-
+	defer file.Close()
+    
+	// check disk space
+    wd, _ := os.Getwd()
+	var stat unix.Statfs_t
+	if err := unix.Statfs(wd, &stat); err != nil {
+		msgHandler.SendResponse(false, "Internal server error")
+		msgHandler.Close()
+		return
+	}
+	availableSpace := stat.Bavail * uint64(stat.Bsize)
+	if request.Size > availableSpace {
+		msgHandler.SendResponse(false, "Not enough disk space")
+		file.Close()
+		os.Remove(request.FileName)
+		msgHandler.Close()
+		return
+	}
+    
+	// send an “OK” response to the client so it knows it can begin sending the file
 	msgHandler.SendResponse(true, "Ready for data")
-	md5 := md5.New()
-	w := io.MultiWriter(file, md5)
-	io.CopyN(w, msgHandler, int64(request.Size)) /* Write and checksum as we go */
+
+	//Receive data stream and store the file
+	h := md5.New()
+	w := io.MultiWriter(file, h)
+	if _, err := io.CopyN(w, msgHandler, int64(request.Size)); err != nil {
+		file.Close()
+		os.Remove(request.FileName)
+		msgHandler.Close()
+		return
+	}
 	file.Close()
 
-	serverCheck := md5.Sum(nil)
-
-	clientCheckMsg, _ := msgHandler.Receive()
+	// Verify its checksum against the checksum sent by the client
+	serverCheck := h.Sum(nil)
+	clientCheckMsg, err := msgHandler.Receive()
+    if err != nil || clientCheckMsg.GetChecksum() == nil {
+		os.Remove(request.FileName)
+		msgHandler.Close()
+		return
+	}
 	clientCheck := clientCheckMsg.GetChecksum().Checksum
 
+	// Respond to the client with the status of the transfer (success or failure)
 	if util.VerifyChecksum(serverCheck, clientCheck) {
 		log.Println("Successfully stored file.")
+		msgHandler.SendResponse(true, "Storage complete")
 	} else {
 		log.Println("FAILED to store file. Invalid checksum.")
+		os.Remove(request.FileName)
+		msgHandler.SendResponse(false, "Invalid checksum")
 	}
+
+	// Disconnect the client
+	msgHandler.Close()
 }
 
 func handleRetrieval(msgHandler *messages.MessageHandler, request *messages.RetrievalRequest) {
@@ -44,19 +84,33 @@ func handleRetrieval(msgHandler *messages.MessageHandler, request *messages.Retr
 	// Get file size and make sure it exists
 	info, err := os.Stat(request.FileName)
 	if err != nil {
-		log.Fatalln(err)
+		msgHandler.SendRetrievalResponse(false, "File not found", 0)
+		msgHandler.Close()
+		return
 	}
-
+    
+	// Send a response back to the client with the file’s size and checksum
 	msgHandler.SendRetrievalResponse(true, "Ready to send", uint64(info.Size()))
 
-	file, _ := os.Open(request.FileName)
-	md5 := md5.New()
-	w := io.MultiWriter(msgHandler, md5)
+	file, err := os.Open(request.FileName)
+	if err != nil {
+		_ = msgHandler.SendRetrievalResponse(false, err.Error(), 0)
+		msgHandler.Close()
+		return
+	}
+	defer file.Close()
+
+	// Begin streaming file to the client
+	h := md5.New()
+	w := io.MultiWriter(msgHandler, h)
 	io.CopyN(w, file, info.Size()) // Checksum and transfer file at same time
 	file.Close()
 
-	checksum := md5.Sum(nil)
+	checksum := h.Sum(nil)
 	msgHandler.SendChecksumVerification(checksum)
+
+	// Disconnect the client
+	msgHandler.Close()
 }
 
 func handleClient(msgHandler *messages.MessageHandler) {
@@ -65,21 +119,22 @@ func handleClient(msgHandler *messages.MessageHandler) {
 	for {
 		wrapper, err := msgHandler.Receive()
 		if err != nil {
-			log.Println(err)
+			return 
 		}
 
 		switch msg := wrapper.Msg.(type) {
 		case *messages.Wrapper_StorageReq:
 			handleStorage(msgHandler, msg.StorageReq)
-			continue
+			return
 		case *messages.Wrapper_RetrievalReq:
 			handleRetrieval(msgHandler, msg.RetrievalReq)
-			continue
+			return 
 		case nil:
 			log.Println("Received an empty message, terminating client")
 			return
 		default:
 			log.Printf("Unexpected message type: %T", msg)
+			return
 		}
 	}
 }
